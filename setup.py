c5e4cee2b4fa43e268222fe1e8ec97955dbaa1c0
nalourie <nalourie@gmail.com>
2014-10-30 18:29:58 -0400

    fixed the lack of test data shipping with the app bug.

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-charitychecker',
-    version='1.1',
+    version='1.2',
     author='Nicholas A. Lourie',
     author_email='developer.nick@kozbox.com',
-    install_requires="django >= 1.6",
+    install_requires="",
     packages=['charitychecker',
               'charitychecker.management',
-              'charitychecker.management.commands'],
+              'charitychecker.management.commands',
+              'charitychecker.test_data'],
+    data_files=[
+        ('charitychecker/test_data',
+         ['charitychecker/test_data/mock-irs-data-after.txt',
+          'charitychecker/test_data/mock-irs-data-before.txt',
+          'charitychecker/test_data/test-data-with-FORGNS.txt',
+          'charitychecker/test_data/test-data-without-FORGNS.txt']),
+    ],
     license='MIT License',
     description=('a small django app to verify information '
                  'about nonprofits using their EINs'),
